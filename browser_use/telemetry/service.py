440ecac0912591907f61781c65da6e4743a24e2f
Zyad <42823027+zyadcodes@users.noreply.github.com>
2025-02-21 13:38:08 -0800

    Browser Use changes

--- browser_use/telemetry/service.py
+++ browser_use/telemetry/service.py
@@ -2,7 +2,6 @@ import posthog
 
 from posthog.ai.langchain import CallbackHandler
 
-
 posthog.project_api_key = "phc_KqAFA7J95snGx0kEDbXhGri2oh2BtLOCs8Ell3TTK3d"
 posthog.host = "https://us.i.posthog.com"
 
