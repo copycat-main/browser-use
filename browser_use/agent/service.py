b6acef77608fb7161c20ec43b9143563f3ff5261
Abhi Balijepalli <abhibalijepalli@gmail.com>
2025-03-19 11:01:25 -0700

    added a callback lambda

--- browser_use/agent/service.py
+++ browser_use/agent/service.py
@@ -526,7 +526,7 @@ class Agent(Generic[Context]):
 
 	# @observe(name='agent.run', ignore_output=True)
 	@time_execution_async('--run (agent)')
-	async def run(self, max_steps: int = 100, is_stopped: bool = False) -> AgentHistoryList:
+	async def run(self, max_steps: int = 100, is_stopped: Callable[[], bool] = lambda: False) -> AgentHistoryList:
 		"""Execute the task with maximum number of steps"""
 		try:
 			self._log_agent_run()
@@ -543,7 +543,7 @@ class Agent(Generic[Context]):
 					break
 
 				# Check control flags before each step
-				if self.state.stopped or is_stopped:
+				if self.state.stopped or is_stopped():
 					logger.info('Agent stopped shdshdsdh')
 					break
 
@@ -580,7 +580,6 @@ class Agent(Generic[Context]):
 
 			if not self.injected_browser and self.browser:
 				await self.browser.close()
-
 	# @observe(name='controller.multi_act')
 	@time_execution_async('--multi-act (agent)')
 	async def multi_act(
