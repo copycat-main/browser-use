753b4c8591d5b92d8dfe42cdfd5a30d2325f020f
Abhi Balijepalli <abhibalijepalli@gmail.com>
2025-04-11 15:18:44 -0700

    download paths

--- browser_use/browser/context.py
+++ browser_use/browser/context.py
@@ -1124,31 +1124,9 @@ class BrowserContext:
 				raise Exception(f'Element: {repr(element_node)} not found')
 
 			async def perform_click(click_func):
-				"""Performs the actual click, handling both download
-				and navigation scenarios."""
-				if self.config.save_downloads_path:
-					try:
-						# Try short-timeout expect_download to detect a file download has been been triggered
-						async with page.expect_download(timeout=5000) as download_info:
-							await click_func()
-						download = await download_info.value
-						# Determine file path
-						suggested_filename = download.suggested_filename
-						unique_filename = await self._get_unique_filename(self.config.save_downloads_path, suggested_filename)
-						download_path = os.path.join(self.config.save_downloads_path, unique_filename)
-						await download.save_as(download_path)
-						logger.debug(f'Download triggered. Saved file to: {download_path}')
-						return download_path
-					except TimeoutError:
-						# If no download is triggered, treat as normal click
-						logger.debug('No download triggered within timeout. Checking navigation...')
-						await page.wait_for_load_state()
-						await self._check_and_handle_navigation(page)
-				else:
-					# Standard click logic if no download is expected
-					await click_func()
-					await page.wait_for_load_state()
-					await self._check_and_handle_navigation(page)
+				await click_func()
+				await page.wait_for_load_state()
+				await self._check_and_handle_navigation(page)
 
 			try:
 				return await perform_click(lambda: element_handle.click(timeout=10000))
